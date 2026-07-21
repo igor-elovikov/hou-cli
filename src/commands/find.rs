a1502fe1a2ee9a99ec839323de9847ea3012df1e
Igor Elovikov <igor.elovikov@gmail.com>
2026-07-21 17:57:11 +0100

    find output improvements

--- src/commands/find.rs
+++ src/commands/find.rs
@@ -1,6 +1,6 @@
 use crate::hou::Context;
 use crate::sidefx::Houdini::Default;
-use crate::sidefx::Product;
+use crate::sidefx::{Product, Release};
 use crate::utils::normalize_version_filter;
 use anyhow::Result;
 use clap::Args;
@@ -18,18 +18,21 @@ impl FindCmd {
         let client = ctx.sidefx_client()?;
         let builds = client.builds(Product::Houdini(Default)).send()?;
 
-        let versions = builds
+        let unique_builds = builds
             .iter()
             .filter(|b| req.matches(&b.version))
-            .map(|b| b.version.clone())
-            .unique()
+            .unique_by(|b| b.version.clone())
             .collect::<Vec<_>>();
 
-        if versions.is_empty() {
+        if unique_builds.is_empty() {
             println!("No version found");
         } else {
-            for version in &versions {
-                println!("{}", version);
+            for b in &unique_builds {
+                let kind = match b.release {
+                    Release::Gold => {"Production"}
+                    Release::Development => {"Daily"}
+                };
+                println!("{} {}", b.version, kind);
             }
         }
 
